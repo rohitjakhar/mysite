1209b1d3e884067311deb1ba4bda07f9f475d57f
Rohit Jakhar <rohitjakhar940@gmail.com>
2022-03-21 18:18:23 +0530

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -76,7 +76,7 @@ const Hero = () => {
             mb={{ base: 2, sm: 0 }}
             size="lg"
             cursor="pointer"
-            href="https://bit.ly/rohit-resume-pdf"
+            href="https://bit.ly/rohit-resume3"
           >
             Resume
           </Button>
