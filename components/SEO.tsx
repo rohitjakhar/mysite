09569fec3d8e4ba363a938c1d89d2a25aac82b36
Rohit Jakhar <rohitjakhar940@gmail.com>
2022-05-07 10:35:23 +0530

    Add Banner

--- components/SEO.tsx
+++ components/SEO.tsx
@@ -16,14 +16,14 @@ const SEO = ({title} : {title : string}) => {
       <meta property="og:url" content="https://rohitjakhar.me/"/>
       <meta property="og:title" content={`Rohit Jakhar — ${title}`}/>
       <meta property="og:description" content="Android Developer."/>
-      <meta property="og:image" content="https://aarush-goyal.github.io/banner.png"/>
+      <meta property="og:image" content="https://github.com/rohitjakhar/rohitjakhar/blob/master/Rohit%20Jakhar%20Banner.png?raw=true"/>
 
 
       <meta property="twitter:card" content="summary_large_image"/>
-      <meta property="twitter:url" content="https://aarush-goyal.github.io/"/>
+      <meta property="twitter:url" content="https://twitter.com/rohitjakhar0"/>
       <meta property="twitter:title" content={`Rohit Jakhar — ${title}`}/>
       <meta property="twitter:description" content="Android Developer."/>
-      <meta property="twitter:image" content="https://aarush-goyal.github.io/banner.png"></meta>
+      <meta property="twitter:image" content="https://github.com/rohitjakhar/rohitjakhar/blob/master/Rohit%20Jakhar%20Banner.png?raw=true"></meta>
     </Head>
   )
 }
